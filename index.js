d9ed186163a8c0841f273c2a95d67e807d97a402
bony075 <bony.075@gmail.com>
2022-10-22 14:18:18 +0600

    add news and catagory data in news server dataa

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+
+const categories = require('./data/category.json');
+
+
 app.get('/', (req, res) => {
     res.send('Api is running');
+
+    app.get('/news-categories', (req, res) => {
+        res.send(categories);
+    });
 
 
 });
